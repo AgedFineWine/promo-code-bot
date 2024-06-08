7c8f8d19b1d842043dccdf14ce95a1f7675124dd
Evan He <eh4359@rit.edu>
2024-06-08 17:32:04 -0400

    updated promo code source

--- src/python-scripts/genshin_codes.py
+++ src/python-scripts/genshin_codes.py
@@ -1,46 +1,68 @@
-"""Module providing return type hint to functions"""
-from typing import List
-from bs4 import BeautifulSoup
-from bs4 import Tag
+"""
+requests is used to send HTTP requests to retrieve web content
+bs4 (BeautifulSoup) is used to parse the HTML file of the web page
+"""
 import requests
+from bs4 import BeautifulSoup
 
-def find_li_tags(soup: BeautifulSoup) -> List[Tag]:
+def is_code(a_string: str) -> bool:
     """
-    Finds and returns all 'li' tags within the first 'ul' tag
-    inside the 'div' with class 'entry-content'
+    Determines whether a string contains whitespaces in between words.
 
-    :param soup: BeautifulSoup object representing the parsed HTML
-    :return: List of 'li' Tag objects
+    :args: a_string (str): any string
+    :returns: a boolean value:
     """
 
-    ancestor_tag = soup.find('div', class_="entry-content")
-    first_ul_tag = ancestor_tag.find('ul')
-    li_tags = first_ul_tag.find_all('li')
-    return li_tags
+    return not any(character.isspace() for character in a_string.strip())
 
-def print_codes(li_tags: List[Tag]) -> None:
+def find_codes_and_rewards(soup: BeautifulSoup) -> dict[str, list[str]]:
     """
-    Prints the number of codes and the text within each 'li' tag
+    Extracts promo codes and their associated rewards from the provided BeautifulSoup object.
 
-    :param li_tags: List of 'li' Tag objects
+    :args: soup (BeautifulSoup): A BeautifulSoup object containing the parsed HTML of the webpage.
+
+    :returns: dict[str, list[str]]: A dictionary where keys are promo codes (str)\
+         and values are lists of rewards (list[str]).
     """
-    number_of_codes = len(li_tags)
-    print(f'There are {number_of_codes} new Genshin Impact codes available:\n')
-    for li_tag in li_tags:
-        msg_array = str(li_tag.text).split('–')
-        print(f"{li_tag.find('strong').text.strip()} - {msg_array[1].replace("’", "'").strip()}")
-    print('\nRedeem the codes here: https://hsr.hoyoverse.com/gift')
 
-def main():
+    cumulative_codes_list = []
+    cumulative_rewards_list = []
+    td_list = soup.find_all('td', attrs={'data-sort-val': True})
+    for td_tag in td_list:
+        code = td_tag.parent.contents[1]
+        if is_code(code.text): # if the code contains words, it is excluded from the list
+            cumulative_codes_list.append(code.text.rstrip('\n'))
+        item_list = td_tag.parent.find_all('span', class_='item')
+        rewards_list = []
+        for item in item_list:
+            rewards_list.append(item.text.strip())
+        cumulative_rewards_list.append(rewards_list)
+    return dict(zip(cumulative_codes_list, cumulative_rewards_list))
+
+def print_out(cumulative_dict: dict) -> None:
     """
-    Finds the genshin impact codes
+    Prints the promo codes and their rewards in a formatted manner for the discord bot to print.
+
+    :args: cumulative_dict (dict[str, list[str]]): A dictionary where keys\
+         are promo codes (str) and values are lists of rewards (list[str]).
     """
-    url = "https://www.pockettactics.com/genshin-impact/codes"
-    status = requests.get(url, timeout=60)
-    soup = BeautifulSoup(status.text, 'html.parser')
 
-    li_tags = find_li_tags(soup)
-    print_codes(li_tags)
+    print(f'There are {len(cumulative_dict)} new codes available to redeem:\n')
+    for codes, rewards in cumulative_dict.items():
+        print(f'{codes} - {', '.join(rewards)}')
+    print('\nRedeem the codes here: https://genshin.hoyoverse.com/en/gift')
 
-if __name__ == "__main__":
+def main() -> None:
+    """
+    Main function that retrieves the webpage, parses it, and prints the promo codes and rewards.
+    """
+
+    url = 'https://genshin-impact.fandom.com/wiki/Promotional_Code'
+    response = requests.get(url, timeout=60)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    cumulative_dict = find_codes_and_rewards(soup)
+    print_out(cumulative_dict)
+
+if __name__ == '__main__':
     main()
--- src/python-scripts/hsr_codes.py
+++ src/python-scripts/hsr_codes.py
@@ -1,46 +1,56 @@
-"""Module providing return type hint to functions"""
-from typing import List
+"""
+requests is used to send HTTP requests to retrieve web content
+bs4 (BeautifulSoup) is used to parse the HTML file of the web page
+"""
 import requests
 from bs4 import BeautifulSoup
-from bs4 import Tag
 
-def find_ul_tags(soup: BeautifulSoup) -> List[Tag]:
+def find_codes_and_rewards(soup: BeautifulSoup) -> dict[str, list[str]]:
     """
-    Finds and returns all 'ul' tags inside the first 'div' with class 'entry-content'
+    Extracts promo codes and their associated rewards from the provided BeautifulSoup object.
 
-    :param soup: BeautifulSoup object representing the parsed HTML
-    :return: List of 'ul' Tag objects
+    :args: soup (BeautifulSoup): A BeautifulSoup object containing the parsed HTML of the webpage.
+
+    :returns: dict[str, list[str]]: A dictionary where keys are promo codes (str)\
+         and values are lists of rewards (list[str]).
     """
+    cumulative_codes_list = []
+    cumulative_rewards_list = []
+    td_list = soup.find_all('td', class_='bg-new')
+    for td_tag in td_list:
+        code = td_tag.parent.contents[0].find('code')
+        cumulative_codes_list.append(code.text)
+        item_list = td_tag.parent.find_all('span', class_='item')
+        rewards_list = []
+        for item in item_list:
+            item_element = item.find('span', class_='item-text')
+            rewards_list.append(item_element.text.strip())
+        cumulative_rewards_list.append(rewards_list)
 
-    target_tag = soup.find('div', class_='entry-content')
-    target_ul = target_tag.find_all('ul')
-    return target_ul
+    return dict(zip(cumulative_codes_list, cumulative_rewards_list))
 
-def print_codes(target_ul: List[Tag]) -> None:
+def print_out(cumulative_dict: dict) -> None:
     """
-    Finds and prints all the 'li' tags within the first target_ul tag list object
+    Prints the promo codes and their rewards in a formatted manner for the discord bot to print.
 
-    :param target_ul:
+    :args: cumulative_dict (dict[str, list[str]]): A dictionary where keys\
+         are promo codes (str) and values are lists of rewards (list[str]).
     """
-
-    number_of_codes = len(target_ul[0].find_all('li'))
-    print(f'There are {number_of_codes} new codes available:\n')
-    for li_tag in target_ul[0].find_all('li'):
-        msg_array = str(li_tag.text).split('–')
-        print(f"{li_tag.find('strong').text.strip()} - {msg_array[1].replace("’", "'").strip()}")
+    print(f'There are {len(cumulative_dict)} new codes available to redeem:\n')
+    for codes, rewards in cumulative_dict.items():
+        print(f'{codes} - {','.join(rewards)}')
     print('\nRedeem the codes here: https://hsr.hoyoverse.com/gift')
 
-def main():
+def main() -> None:
     """
-    Finds the honkai star rail codes
+    Main function that retrieves the webpage, parses it, and prints the promo codes and rewards.
     """
+    url = 'https://honkai-star-rail.fandom.com/wiki/Redemption_Code'
+    response = requests.get(url, timeout=60)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-    url = 'https://www.pockettactics.com/honkai-star-rail/codes'
-    status = requests.get(url, timeout=60)
-    soup = BeautifulSoup(status.text, 'html.parser')
+    cumulative_dict = find_codes_and_rewards(soup)
+    print_out(cumulative_dict)
 
-    all_target_uls = find_ul_tags(soup)
-    print_codes(all_target_uls)
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
